56dc20aae114fb475d7e4414bfeddcb19f879620
atiqur <toatikbd@gmail.com>
2021-03-16 23:55:35 +0600

    clone home pc and input some code in left sidebar

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -1,6 +1,44 @@
 <template>
     <div class="leftsidebar">
-        <h1>back to page</h1>
+        <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-3">
+                  <div class="card">
+                    <img class="card-img-top" src="img/1.png" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="card">
+                    <img class="card-img-top" src="img/2.png" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="card">
+                    <img class="card-img-top" src="img/1.png" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-3">
+                  <div class="card">
+                    <img class="card-img-top" src="img/2.png" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick</p>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
     </div>
     
 </template>
